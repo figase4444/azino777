93e2f5e74ee88254de39550b99c8f8f0346e74cf
figase4444 <l@obana.info>
2024-05-30 08:56:10 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express")
+const path = require("path")
+const bodyParser = require("body-parser")
+const gamesRouter = require("./routes/games")
+const categoriesRouter = require("./routes/categories")
+const usersRouter = require("./routes/users")
+const PORT = 3000
+const app = express()
+//connectToDatabase()
+app.use(
+    bodyParser.json(),
+    express.static(path.join(__dirname, "public")),
+    usersRouter,
+    gamesRouter,
+    categoriesRouter
+
+)
+app.listen(PORT, () => {
+    console.log(`Server is running at PORT http://localhost:${PORT}`)
+})
